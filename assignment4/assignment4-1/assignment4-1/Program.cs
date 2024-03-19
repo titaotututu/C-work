d88a7ea8c26624d7ad187716fe55abb42e346888
titaotututu <476589881@qq.com>
2024-03-19 21:53:49 +0800

    add assignment4

---
+++ assignment4/assignment4-1/assignment4-1/Program.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * 为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。
+ * 通过调用这个方法打印链表元素，
+ * 求最大值、最小值和求和（使用lambda表达式实现）
+ *
+ */
+
+using System;
+using System.Diagnostics;
+//定义链表节点
+public class Node<T>
+{
+    public Node<T> next{get;set;}  //next指针
+    public T data { get; set; }
+
+    public Node(T t)
+    {
+        next = null;
+        data = t;
+    }
+}
+
+//定义泛型链表类
+public class GenericList<T>
+{
+    private Node<T> tail;  //定义尾结点
+    private Node<T> head;  //定义头结点
+    public GenericList()  //初始化
+    {
+        tail = null;
+        head = null;
+    }
+    public Node<T> Head  //获取头节点的方法
+    {
+        get => head;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.next = n;
+            tail= n;    
+        }
+    }
+    //ForEach方法
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current= head;
+        while (current!=null)
+        {
+            action(current.data);
+            current= current.next;  
+        }
+
+    }
+}
+
+//主程序
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //创建一个int类型的链表对象
+        GenericList<int>LinkList= new GenericList<int>();
+        LinkList.Add(99);
+        LinkList.Add(2);
+        LinkList.Add(2);
+        LinkList.Add(8);
+        LinkList.Add(7);
+        LinkList.Add(9);
+        LinkList.Add(30);
+        Console.WriteLine("链表中的元素为：");
+        LinkList.ForEach(data=>Console.WriteLine(data));  //打印链表保存的数据
+        int max =LinkList.Head.data;
+        int min =LinkList.Head.data;
+        int sum = 0;
+        LinkList.ForEach(data =>
+        {
+            if (data > max)
+            {
+                max = data;
+            }
+            if (data < min)
+            {
+                min = data;
+            }
+            sum += data;
+        });
+
+        Console.WriteLine("Max: " + max);
+        Console.WriteLine("Min: " + min);
+        Console.WriteLine("Sum: " + sum);
+
+    }
+}
+
---
+++ assignment4/assignment4-2/assignment4-2/Program.cs
@@ -0,0 +1,65 @@
+﻿
+/*
+ 使用事件机制，模拟实现一个闹钟功能。
+闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+在闹钟走时时或者响铃时，在控制台显示提示信息。
+
+ */
+
+using System;
+using System.Threading;
+
+public class AlarmClock  //闹钟类
+{
+    //定义闹钟Tick事件；
+    public event EventHandler Tick;
+    //定义响铃事件Alarm
+    public event EventHandler Alarm;
+    //触发Tick
+    protected virtual void ClockTick(EventArgs e)
+    {
+        if (Tick != null) { 
+        Tick(this, e);}
+
+    }
+    //触发Alarm
+    protected virtual void ClockAlarm(EventArgs e)
+    {
+        if(Alarm != null)
+        {
+            Alarm(this, e);
+        }
+    }
+
+    //启动闹钟
+    public void Start()
+    {
+        for(int i = 0; i < 10; i++)  //用于模拟时间的流逝,十次循环后，闹钟响
+        {
+            ClockTick(EventArgs.Empty);
+            Thread.Sleep(1000);  //每秒Tick一下
+        }
+        ClockAlarm(EventArgs.Empty);// 触发响铃事件
+
+    }
+}
+
+//主程序
+class Program
+{
+    public static void Main(string[] args)
+    {
+        AlarmClock clock= new AlarmClock(); //创建一个闹钟对象
+        //订阅Tick事件
+        clock.Tick += (sender, e) => Console.WriteLine("Ticking.....");  //两个参数表示事件的发送者和事件参数
+        //订阅Alarm事件
+        clock.Alarm += (sender, e) => Console.WriteLine("Alarm ringing .....");
+
+        Console.WriteLine("starting the alarm clock");
+        clock.Start();
+        Console.WriteLine("按任意键退出");
+        Console.ReadKey();
+
+
+    }
+}
